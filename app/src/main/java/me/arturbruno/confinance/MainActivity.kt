55d41d536c2246367a948d014c4c149cf05035b4
Artur Bruno <arturjuliao20@gmail.com>
2021-11-18 15:01:55 -0300

    feat: Project created

---
+++ app/src/main/java/me/arturbruno/confinance/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.arturbruno.confinance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
