a3d82516c90393f477bc7c396204b7a5a1b7b5c6
Artur Bruno <arturjuliao20@gmail.com>
2022-01-11 10:11:21 -0300

    feat(ui): Created screen to show credit card details

---
+++ app/src/main/java/me/arturbruno/confinance/views/CreditCardDetailsActivity.kt
@@ -0,0 +1,15 @@
+package me.arturbruno.confinance.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.arturbruno.confinance.R
+
+class CreditCardDetailsActivity : AppCompatActivity() {
+
+//    private val binding:
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit_card_details)
+    }
+}
