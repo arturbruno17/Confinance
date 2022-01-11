36964fdae423405ee5ae437d089da86d6c00d1e9
Artur Bruno <arturjuliao20@gmail.com>
2022-01-11 09:16:32 -0300

    feat(ui): Created screen to show bank account details

---
+++ app/src/main/java/me/arturbruno/confinance/views/BankAccountDetailsActivity.kt
@@ -0,0 +1,18 @@
+package me.arturbruno.confinance.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.arturbruno.confinance.R
+import me.arturbruno.confinance.databinding.ActivityBankAccountDetailsBinding
+
+class BankAccountDetailsActivity : AppCompatActivity() {
+
+    private val binding: ActivityBankAccountDetailsBinding by lazy {
+        ActivityBankAccountDetailsBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
